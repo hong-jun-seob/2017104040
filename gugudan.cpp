7d56eb23e3b0050dd4adf5a730d3bf66d9380589
홍준섭 <wnstjqek123@khu.ac.kr>
2020-10-16 10:41:34 +0900

    gugudan.cpp

---
+++ gugudan.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= 8; i++)
+	{
+		cout <<n<<" * "<< i<< " = " <<n * i << endl;
+	}
+
+	return 0;
+}
